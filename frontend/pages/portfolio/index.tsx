d99cbdc86048d11ea3b23ec0ad787c0b504c37e6
Yusuf ŞAL <yusuf.sal.1996@gmail.com>
2023-03-15 06:35:27 +0300

    Portfolio page has been completed..

--- frontend/pages/portfolio/index.tsx
+++ frontend/pages/portfolio/index.tsx
@@ -1,4 +1,9 @@
-import { IoChevronDown } from "react-icons/io5";
+import Image from "next/image";
+import { FiArrowUpRight } from "react-icons/fi";
+import { IoLogoAppleAppstore, IoLogoGooglePlaystore } from "react-icons/io5";
+import personalPreview from "../../public/personal.png";
+import recipePreview from "../../public/recipe.png";
+import yogaPreview from "../../public/yoga.png";
 
 export default function Portfolio() {
   return (
@@ -8,272 +13,86 @@ export default function Portfolio() {
       </header>
 
       <section className="projects">
-        <ul className="filter-list">
-          <li className="filter-item">
-            <button className="active" data-filter-btn>
-              All
-            </button>
-          </li>
-
-          <li className="filter-item">
-            <button data-filter-btn>Web design</button>
-          </li>
-
-          <li className="filter-item">
-            <button data-filter-btn>Applications</button>
-          </li>
-
-          <li className="filter-item">
-            <button data-filter-btn>Web development</button>
-          </li>
-        </ul>
-
-        <div className="filter-select-box">
-          <button className="filter-select" data-select>
-            <div className="select-value" data-selecct-value>
-              Select category
-            </div>
-
-            <div className="select-icon">
-              <IoChevronDown />
-              {/* <ion-icon name="chevron-down"></ion-icon> */}
-            </div>
-          </button>
-
-          <ul className="select-list">
-            <li className="select-item">
-              <button data-select-item>All</button>
-            </li>
-
-            <li className="select-item">
-              <button data-select-item>Web design</button>
-            </li>
-
-            <li className="select-item">
-              <button data-select-item>Applications</button>
-            </li>
-
-            <li className="select-item">
-              <button data-select-item>Web development</button>
-            </li>
-          </ul>
-        </div>
-
         <ul className="project-list">
           <li
             className="project-item  active"
             data-filter-item
-            data-category="web development"
+            data-category="application"
           >
-            <a href="#">
+            <div>
               <figure className="project-img">
                 <div className="project-item-icon-box">
-                  {/*  <ion-icon name="eye-outline"></ion-icon> */}
+                  <a href="https://play.google.com/store/apps/details?id=com.sahajayogameditasyon.sahajayoga&hl=en&gl=US">
+                    <IoLogoGooglePlaystore />
+                  </a>
+
+                  <a href="https://apps.apple.com/us/app/sahaja-yoga-meditasyon/id1556526076">
+                    <IoLogoAppleAppstore />
+                  </a>
+                  <a href="https://sahajayogameditasyon.com/">
+                    <FiArrowUpRight />
+                  </a>
                 </div>
 
-                <img
-                  src="./assets/images/project-1.jpg"
-                  alt="finance"
+                <Image
+                  src={yogaPreview}
+                  alt="Meditaion & Yoga"
                   loading="lazy"
                 />
               </figure>
 
-              <h3 className="project-title">Finance</h3>
+              <h3 className="project-title">Sahaja Meditation & Yoga</h3>
 
-              <p className="project-category">Web development</p>
-            </a>
+              <p className="project-category">Mobile Application</p>
+            </div>
           </li>
-
           <li
             className="project-item  active"
             data-filter-item
-            data-category="web development"
+            data-category="application"
           >
-            <a href="#">
+            <div>
               <figure className="project-img">
                 <div className="project-item-icon-box">
-                  {/*  <ion-icon name="eye-outline"></ion-icon> */}
+                  <a href="https://play.google.com/store/apps/details?id=com.shirimatajinirmaladevi.recipes">
+                    <IoLogoGooglePlaystore />
+                  </a>
+
+                  <a href="https://apps.apple.com/gb/app/shrimatajinirmaladevisrecipes/id6444897780">
+                    <IoLogoAppleAppstore />
+                  </a>
                 </div>
 
-                <img
-                  src="./assets/images/project-2.png"
-                  alt="orizon"
-                  loading="lazy"
-                />
+                <Image src={recipePreview} alt="Shiri " loading="lazy" />
               </figure>
 
-              <h3 className="project-title">Orizon</h3>
+              <h3 className="project-title">
+                Shri Mataji Nirmala Devi&prime;s Recipes
+              </h3>
 
-              <p className="project-category">Web development</p>
-            </a>
+              <p className="project-category">Mobile Application</p>
+            </div>
           </li>
-
           <li
             className="project-item  active"
             data-filter-item
-            data-category="web design"
+            data-category="application"
           >
-            <a href="#">
+            <div>
               <figure className="project-img">
                 <div className="project-item-icon-box">
-                  {/*  <ion-icon name="eye-outline"></ion-icon> */}
+                  <a href="https://www.yusufsal.com/">
+                    <FiArrowUpRight />
+                  </a>
                 </div>
 
-                <img
-                  src="./assets/images/project-3.jpg"
-                  alt="fundo"
-                  loading="lazy"
-                />
+                <Image src={personalPreview} alt="Shiri " loading="lazy" />
               </figure>
 
-              <h3 className="project-title">Fundo</h3>
+              <h3 className="project-title">Yusuf ŞAL Personal Site</h3>
 
-              <p className="project-category">Web design</p>
-            </a>
-          </li>
-
-          <li
-            className="project-item  active"
-            data-filter-item
-            data-category="applications"
-          >
-            <a href="#">
-              <figure className="project-img">
-                <div className="project-item-icon-box">
-                  {/* <ion-icon name="eye-outline"></ion-icon> */}
-                </div>
-
-                <img
-                  src="./assets/images/project-4.png"
-                  alt="brawlhalla"
-                  loading="lazy"
-                />
-              </figure>
-
-              <h3 className="project-title">Brawlhalla</h3>
-
-              <p className="project-category">Applications</p>
-            </a>
-          </li>
-
-          <li
-            className="project-item  active"
-            data-filter-item
-            data-category="web design"
-          >
-            <a href="#">
-              <figure className="project-img">
-                <div className="project-item-icon-box">
-                  {/* <ion-icon name="eye-outline"></ion-icon> */}
-                </div>
-
-                <img
-                  src="./assets/images/project-5.png"
-                  alt="dsm."
-                  loading="lazy"
-                />
-              </figure>
-
-              <h3 className="project-title">DSM.</h3>
-
-              <p className="project-category">Web design</p>
-            </a>
-          </li>
-
-          <li
-            className="project-item  active"
-            data-filter-item
-            data-category="web design"
-          >
-            <a href="#">
-              <figure className="project-img">
-                <div className="project-item-icon-box">
-                  {/* <ion-icon name="eye-outline"></ion-icon> */}
-                </div>
-
-                <img
-                  src="./assets/images/project-6.png"
-                  alt="metaspark"
-                  loading="lazy"
-                />
-              </figure>
-
-              <h3 className="project-title">MetaSpark</h3>
-
-              <p className="project-category">Web design</p>
-            </a>
-          </li>
-
-          <li
-            className="project-item  active"
-            data-filter-item
-            data-category="web development"
-          >
-            <a href="#">
-              <figure className="project-img">
-                <div className="project-item-icon-box">
-                  {/* <ion-icon name="eye-outline"></ion-icon> */}
-                </div>
-
-                <img
-                  src="./assets/images/project-7.png"
-                  alt="summary"
-                  loading="lazy"
-                />
-              </figure>
-
-              <h3 className="project-title">Summary</h3>
-
-              <p className="project-category">Web development</p>
-            </a>
-          </li>
-
-          <li
-            className="project-item  active"
-            data-filter-item
-            data-category="applications"
-          >
-            <a href="#">
-              <figure className="project-img">
-                <div className="project-item-icon-box">
-                  {/* <ion-icon name="eye-outline"></ion-icon> */}
-                </div>
-
-                <img
-                  src="./assets/images/project-8.jpg"
-                  alt="task manager"
-                  loading="lazy"
-                />
-              </figure>
-
-              <h3 className="project-title">Task Manager</h3>
-
-              <p className="project-category">Applications</p>
-            </a>
-          </li>
-
-          <li
-            className="project-item  active"
-            data-filter-item
-            data-category="web development"
-          >
-            <a href="#">
-              <figure className="project-img">
-                <div className="project-item-icon-box">
-                  {/* <ion-icon name="eye-outline"></ion-icon> */}
-                </div>
-
-                <img
-                  src="./assets/images/project-9.png"
-                  alt="arrival"
-                  loading="lazy"
-                />
-              </figure>
-
-              <h3 className="project-title">Arrival</h3>
-
-              <p className="project-category">Web development</p>
-            </a>
+              <p className="project-category">Web Page</p>
+            </div>
           </li>
         </ul>
       </section>
