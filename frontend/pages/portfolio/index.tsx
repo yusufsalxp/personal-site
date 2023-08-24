0ae3ba071182372aee8a9a31d9c59f909708dad7
Yusuf ŞAL <yusuf.sal.1996@gmail.com>
2023-08-24 17:01:46 +0300

    Portfolio has been refactored...

--- frontend/pages/portfolio/index.tsx
+++ frontend/pages/portfolio/index.tsx
@@ -1,14 +1,54 @@
 import Head from "next/head";
 import Image from "next/image";
-import { FiArrowUpRight } from "react-icons/fi";
-import {
-  IoLogoAppleAppstore,
-  IoLogoGithub,
-  IoLogoGooglePlaystore,
-} from "react-icons/io5";
+import * as IO5Icons from "react-icons/io5";
 import personalPreview from "../../public/personal.png";
 import recipePreview from "../../public/recipe.png";
 import yogaPreview from "../../public/yoga.png";
+
+const CustomIO5Icon = ({ name }: { name: string }) => {
+  const FaIcon = IO5Icons[name];
+  if (!FaIcon) return <p>Icon not found!</p>;
+
+  return <FaIcon />;
+};
+
+function ProjectItem({
+  title,
+  category,
+  links,
+  image,
+}: {
+  title: string;
+  category: string;
+  image: any;
+  links: { icon: string; href: string }[];
+}) {
+  return (
+    <li
+      className="project-item  active"
+      data-filter-item
+      data-category="application"
+    >
+      <div>
+        <figure className="project-img">
+          <div className="project-item-icon-box">
+            {links.map(({ icon, href }) => (
+              <a href={href}>
+                <CustomIO5Icon name={icon} />
+              </a>
+            ))}
+          </div>
+
+          <Image src={image} alt={title} loading="lazy" />
+        </figure>
+
+        <h3 className="project-title">{title}</h3>
+
+        <p className="project-category">{category}</p>
+      </div>
+    </li>
+  );
+}
 
 export default function Portfolio() {
   return (
@@ -23,89 +63,55 @@ export default function Portfolio() {
 
         <section className="projects">
           <ul className="project-list">
-            <li
-              className="project-item  active"
-              data-filter-item
-              data-category="application"
-            >
-              <div>
-                <figure className="project-img">
-                  <div className="project-item-icon-box">
-                    <a href="https://play.google.com/store/apps/details?id=com.sahajayogameditasyon.sahajayoga&hl=en&gl=US">
-                      <IoLogoGooglePlaystore />
-                    </a>
-
-                    <a href="https://apps.apple.com/us/app/sahaja-yoga-meditasyon/id1556526076">
-                      <IoLogoAppleAppstore />
-                    </a>
-                    <a href="https://sahajayogameditasyon.com/">
-                      <FiArrowUpRight />
-                    </a>
-                  </div>
-
-                  <Image
-                    src={yogaPreview}
-                    alt="Meditaion & Yoga"
-                    loading="lazy"
-                  />
-                </figure>
-
-                <h3 className="project-title">Sahaja Meditation & Yoga</h3>
-
-                <p className="project-category">Mobile Application</p>
-              </div>
-            </li>
-            <li
-              className="project-item  active"
-              data-filter-item
-              data-category="application"
-            >
-              <div>
-                <figure className="project-img">
-                  <div className="project-item-icon-box">
-                    <a href="https://play.google.com/store/apps/details?id=com.shirimatajinirmaladevi.recipes">
-                      <IoLogoGooglePlaystore />
-                    </a>
-
-                    <a href="https://apps.apple.com/gb/app/shrimatajinirmaladevisrecipes/id6444897780">
-                      <IoLogoAppleAppstore />
-                    </a>
-                  </div>
-
-                  <Image src={recipePreview} alt="Shiri " loading="lazy" />
-                </figure>
-
-                <h3 className="project-title">
-                  Shri Mataji Nirmala Devi&prime;s Recipes
-                </h3>
-
-                <p className="project-category">Mobile Application</p>
-              </div>
-            </li>
-            <li
-              className="project-item  active"
-              data-filter-item
-              data-category="application"
-            >
-              <div>
-                <figure className="project-img">
-                  <div className="project-item-icon-box">
-                    <a href="https://github.com/JosephShall/personal-site">
-                      <IoLogoGithub />
-                    </a>
-                    <a href="https://yusufsal.com/">
-                      <FiArrowUpRight />
-                    </a>
-                  </div>
-
-                  <Image src={personalPreview} alt="Personal" loading="lazy" />
-                </figure>
-
-                <h3 className="project-title">Yusuf ŞAL Personal Site</h3>
-
-                <p className="project-category">Web Page</p>
-              </div>
-            </li>
+            <ProjectItem
+              title="Sahaja Meditation & Yoga"
+              category="Mobile Application"
+              image={yogaPreview}
+              links={[
+                {
+                  href: "https://play.google.com/store/apps/details?id=com.sahajayogameditasyon.sahajayoga&hl=en&gl=US",
+                  icon: "IoLogoGooglePlaystore",
+                },
+                {
+                  href: "https://apps.apple.com/us/app/sahaja-yoga-meditasyon/id1556526076",
+                  icon: "IoLogoAppleAppstore",
+                },
+                {
+                  href: "https://sahajayogameditasyon.com/",
+                  icon: "IoLinkOutline",
+                },
+              ]}
+            />
+            <ProjectItem
+              title="Shri Mataji Nirmala Devi's Recipes"
+              category="Mobile Application"
+              image={recipePreview}
+              links={[
+                {
+                  href: "https://play.google.com/store/apps/details?id=com.shirimatajinirmaladevi.recipes",
+                  icon: "IoLogoGooglePlaystore",
+                },
+                {
+                  href: "https://apps.apple.com/gb/app/shrimatajinirmaladevisrecipes/id6444897780",
+                  icon: "IoLogoAppleAppstore",
+                },
+              ]}
+            />
+            <ProjectItem
+              title="Yusuf ŞAL Personal Site"
+              category="Web Page"
+              image={personalPreview}
+              links={[
+                {
+                  href: "https://github.com/JosephShall/personal-site",
+                  icon: "IoLogoGithub",
+                },
+                {
+                  href: "https://yusufsal.com/",
+                  icon: "IoLinkOutline",
+                },
+              ]}
+            />
           </ul>
         </section>
       </article>
